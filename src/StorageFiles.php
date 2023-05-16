8d2b5880a96a5985868f1d1045bb104621e0d7a3
Vadim <vadimbudrik@gmail.com>
2023-05-16 21:01:34 +0300

    сonverting all paths to a hierarchical array

--- src/StorageFiles.php
+++ src/StorageFiles.php
@@ -4,6 +4,7 @@ namespace Bazarov392;
 
 use Bazarov392\StorageFile;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 
 final class StorageFiles
 {
@@ -13,7 +14,15 @@ final class StorageFiles
             ->where('deletion_date', '!=', null)
             ->where('deletion_date', '<', Carbon::now());
 
-        if($queryDelete->count() > 0) $queryDelete->delete();
+        if($queryDelete->count() > 0) 
+        {
+            $files = $queryDelete->select('path', 'file_id')->get();
+            foreach($files as $file)
+            {
+                $this->deleteFromCache($file->path);
+                $file->delete();
+            }
+        }
     }
 
     public function containsPath(string $path): bool
@@ -38,6 +47,7 @@ final class StorageFiles
 
     public function write(string $path, string $contents, Carbon|null $deletionDate = null, ?array $info = null): StorageFile
     {
+        $this->addToCache($path);
         $file = StorageFile::updateOrCreate([
             'path' => $path
         ],
@@ -56,10 +66,12 @@ final class StorageFiles
     {
         if($pathOrModel instanceof StorageFile)
         {
+            $this->deleteFromCache($pathOrModel->path);
             $pathOrModel->delete();
         }
         else
         {
+            $this->deleteFromCache($pathOrModel);
             $query = StorageFile::where('path', $pathOrModel);
             if($query->exists()) $query->delete();
         }
@@ -71,5 +83,99 @@ final class StorageFiles
         return ($query->exists())
             ? $query->pluck('path')->toArray()
             : [];
+    }
+
+    private function getPathInfo($path): array
+    {
+        preg_match("/^(.+)?(\/)(.+)$/", $path, $matchesPath);
+        return [
+            'folder' => ($matchesPath[1] == '') ? $matchesPath[2] : $matchesPath[1],
+            'file' => basename($path)
+        ];
+    }
+
+    private function addToCache($path): void
+    {
+        $pathInfo = $this->getPathInfo($path);
+        $cachePath = "dbf_{$pathInfo['folder']}";
+        $cache = Cache::get($cachePath);
+        if($cache !== null)
+        {
+            if(!isset($cache[$pathInfo['file']]))
+            {
+                $cache[$pathInfo['file']] = 'file';
+                Cache::put($cachePath, $cache);
+            }
+            
+        }
+        else Cache::put($cachePath, [
+            $pathInfo['file'] => 'file'
+        ]);
+    }
+
+    private function deleteFromCache($path): void
+    {
+        $pathInfo = $this->getPathInfo($path);
+        $cachePath = "dbf_{$pathInfo['folder']}";
+        $cache = Cache::get($cachePath);
+        if($cache !== null)
+        {
+            if(isset($cache[$pathInfo['file']]))
+            {
+                unset($cache[$pathInfo['file']]);
+                if(count($cache) > 0) Cache::put($cachePath, $cache);
+                else Cache::forget($cachePath);
+            }
+        }
+    }
+
+    public function hierarchicalArray(): array
+    {
+        $paths = $this->getList('/');
+        $result = [];
+        foreach ($paths as $path) {
+        $parts = explode('/', $path); 
+
+        $current = &$result;
+
+        foreach ($parts as $part) {
+            if (!isset($current[$part])) {
+            $current[$part] = [];
+            }
+
+            $current = &$current[$part]; 
+        }
+
+        if(count($current) == 0) $current = $path;
+        else $current[] = $path; 
+        }
+
+        return ['/' => $result['']];
+    }
+
+    public function reloadCache(): void
+    {
+        $result = $this->hierarchicalArray()['/'];
+        $cache = $this->cacheFoldersContent($result, '');
+        foreach($cache as $key => $value)
+            Cache::put('dbf_'.$key, $value);
+    }
+
+    private function cacheFoldersContent(array $folders, string $path, array &$cache = []): array
+    {
+        foreach ($folders as $key => $value) {
+            $currentPath = $path . '/' . $key;
+            if (is_array($value)) {
+                $cache[$currentPath] =
+                    array_map(fn ($item) => is_array($item) ? 'folder' : 'file', $value);
+                cacheFoldersContent($value, ($currentPath == '/' ? '' : $currentPath), $cache); // Рекурсивно кэшируем содержимое вложенных папок
+            }
+        }
+        return $cache;
+    }
+
+    public function getContentsFolder(string $path): array|null
+    {
+        return Cache::get('dbf_'.$path);
     }
 }
